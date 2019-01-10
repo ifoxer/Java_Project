66a316ea739fe0aac0a94c236fc84f08df8d94a9
ifoxer <romanliseykin@gmail.com>
2019-01-10 12:46:51 +0300

    пример типа данных long

--- src/typeClass.java
+++ src/typeClass.java
@@ -1,2 +1,17 @@
 public class typeClass {
+
+
+    public static void main(String args[]) {
+
+        long ci;
+
+        long im;
+
+        im = 5280 * 12;
+
+        ci = im * im * im;
+
+        System.out.println("B одной кубической миле содержится " + ci +" кубических дюймов");
+
+    }
 }
