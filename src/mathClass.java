26a89c59398432d6a7f76489790055d3f6e2bb32
ifoxer <romanliseykin@gmail.com>
2019-01-10 15:42:03 +0300

    добавлен пример арифметических операций

--- src/mathClass.java
+++ src/mathClass.java
@@ -1,2 +1,30 @@
 public class mathClass {
+
+
+
+        public static void main(String args[]) {
+
+            int iresult, irem;
+
+            double dresult, drem;
+
+            iresult = 10 / 3;
+
+            irem = 10 % 3;
+
+            dresult = 10.0 / 3.0;
+
+            drem = 10.0 % 3.0;
+
+            System.out.println("Peзyльтaт и остаток от деления 10 / 3: " +
+
+                    iresult + " " + irem);
+
+            System.out.println("Peзyльтaт и остаток от деления 10.0 / 3.0: "
+
+                    + dresult + " " + drem);
+
+        }
+
+
 }
